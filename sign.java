5256b2612fc500f30122625b15af6a5450953376
13BQ1A0538 <gowthami.chintha26@gmail.com>
2017-03-30 20:26:38 +0530

    Add files via upload

---
+++ sign.java
@@ -0,0 +1,14 @@
+package com.example.lenovo.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign);
+    }
+}
